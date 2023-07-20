921367951fdcfb26fc17e89ea8588d432c61930b
Susie Gordon <susieyoungchoi@gmail.com>
2023-07-20 14:26:19 -0400

    create movie grid css

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,13 @@
 function Movie(props) {
     return <div className="Movie">
-        
-            <h2 className="title">{props.movie.name}</h2>
-            <img className="image" src={props.movie.image} alt={props.movie.image} />
-            <div className="info">
-                <p>{props.movie.year}</p>
-                <p>{props.movie.rating}</p>
-            </div>
-            
+
+                <div className="card">
+                    <h2 className="title">{props.movie.name}</h2>
+                    <img className="image" src={props.movie.image} alt={props.movie.image} />
+                    <p>{props.movie.year}</p>
+                    <p>{props.movie.rating}</p>
+                </div>
+     
     </div>
 }
 
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,11 @@ function Movies(props) {
     return <div className="Movies">
         <h1>Movies</h1>
 
+        <div className='Movies-grid'>
         {props.movies.map((item, index) => {
             return <Movie movie={item} key={item.name}/>
-        })}
+            })}
+            </div>
     </div>
 }
 
